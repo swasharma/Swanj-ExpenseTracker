c6e78fb346d5506a93da49e9dee568ed33e78058
swasharma <sharma.swapnil6@gmail.com>
2022-07-04 15:55:51 +0530

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import AddExpense from './components/AddExpense';
+import img2 from './components/Gutu_Papa';
 import './style.css';
 
 export default function App() {
   return (
     <>
+      <img
+        src="https://m.media-amazon.com/images/I/61-l8S81xVL._SL1500_.jpg"
+        height="100px"
+        width="100px"
+      />
       <AddExpense />
     </>
   );
--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import img1 from './Gutu_Papa.jpg';
 
 function AddExpense() {
   return (
     <div>
-      <img src={img1} alt="Gutu And Papa" />
       <h1>
-        I LOVE U PAPA <br /> I Miss You So Much
+        I LOVE U PAPA <br /> I Miss You So Much <br />
       </h1>
     </div>
   );
