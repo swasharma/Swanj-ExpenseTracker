8f42c5642c1a2851e0ee87ded65b4c785606806e
swasharma <sharma.swapnil6@gmail.com>
2022-07-04 16:20:13 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export default function App() {
     <>
       <img
         src="https://lh3.googleusercontent.com/pw/AM-JKLX0uYYwy7tSM0WNpcZEVvDQXNX9ClJU52qvw0kElnQx3COcCWP_3njr8XOw9Ow9pMKjAmNfCPQN9CfyfFE96xOFV6bzgZ6ffcG4QRvE9UUq_h1_ZZy3KnakixjJBIrTeK0XyqHhdO-Dtu5RZ6G7KQudSQ=w678-h903-no?authuser=0"
-        height="300px"
-        width="300px"
+        height="350px"
+        width="350px"
       />
       <AddExpense />
     </>
