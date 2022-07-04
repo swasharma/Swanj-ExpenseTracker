e2455c6b4e940222f9c2034bdc42de2c2021ba29
swasharma <sharma.swapnil6@gmail.com>
2022-07-04 15:37:00 +0530

    1 file modified, 1 added, 1 deleted

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import img1 from './Gutu_Papa.jpg';
 
 function AddExpense() {
   return (
     <div>
-      <img />
+      <img src={img1} alt="Gutu And Papa" />
       <h1>
         I LOVE U PAPA <br /> I Miss You So Much
       </h1>
